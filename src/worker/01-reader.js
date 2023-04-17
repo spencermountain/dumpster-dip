47ffdc254a04e882cd2406477e90397b981a171e
mxunknown <mxunknown@gmail.com>
2023-04-17 12:27:22 +0300

    Whoops forgot it was a promise

--- src/worker/01-reader.js
+++ src/worker/01-reader.js
@@ -37,37 +37,34 @@ const readWiki = function (opts, eachPage) {
   const percent = 100 / workers;
   const start = percent * index;
   const end = start + percent;
-  const language = findDbName(file);
-
-  // console.log(`#${index}  - from ${start} to ${end}`)
-  const driver = {
-    file: file,
-    start: `${start}%`,
-    end: `${end}%`,
-    splitter: '</page>',
-    each: (xml, resume) => {
-      let pageTitle = "Unknown page"
-      try {
-        const meta = parseXml(xml);
-        pageTitle = meta.title
-        // console.log("worker", opts.index, "processing", meta.title);
-        meta.wiki = decode(meta.wiki);
-        meta.language = language;
-        eachPage(meta)
-      } catch(e) {
-        console.log(red(`Worker ${opts.index} couldn't process ${pageTitle}: got error ${e}`));
+  return findDbName(file).then(language => {
+    const driver = {
+      file: file,
+      start: `${start}%`,
+      end: `${end}%`,
+      splitter: '</page>',
+      each: (xml, resume) => {
+        let pageTitle = "Unknown page"
+        try {
+          const meta = parseXml(xml);
+          pageTitle = meta.title
+          meta.wiki = decode(meta.wiki);
+          meta.language = language;
+          eachPage(meta)
+        } catch(e) {
+          console.log(red(`Worker ${opts.index} couldn't process ${pageTitle}: got error ${e}`));
+        }
+        resume();
       }
-      resume();
-    }
-  };
-  const p = sundayDriver(driver);
-  p.catch(err => {
-    console.log(red('\n\n========== Worker error!  ====='));
-    console.log('🚨       worker #' + opts.index + '           🚨');
-    console.log(err);
-    console.log('\n\n');
+    };
+    const p = sundayDriver(driver);
+    p.catch(err => {
+      console.log(red('\n\n========== Worker error!  ====='));
+      console.log('🚨       worker #' + opts.index + '           🚨');
+      console.log(err);
+      console.log('\n\n');
+    });
+    return p;
   });
-  return p
-
 }
 export default readWiki
